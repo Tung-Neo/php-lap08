096558a69417478b2e3436a9ba33a3609756dab6
Tung-Neo <62163195+Tung-Neo@users.noreply.github.com>
2020-07-13 20:15:58 +0700

    lap08

---
+++ Session21/Snippet2.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+$_SESSION['myname'] = 'Jessica';
+?>
+<html>
+<head>
+    <title>Session</title>
+</head>
+<body>
+<a href="Snippet3.php">Homepage of MyPage.com</a>
+</body>
+</html>
